2e8349aabda32ed8880495f1b4322416eb31dc97
Jan-Hendrik Willms <tleilax+github@gmail.com>
2021-09-08 09:07:33 +0200

    fix typo, fixes #152

--- app/views/files/_overview.php
+++ app/views/files/_overview.php
@@ -45,7 +45,7 @@
                      :files="files"
                      :topfolder="topfolder"
                      enable_table_filter="false"
-                     table_title="<?= _('Persönlichr Dateibereich') ?>"
+                     table_title="<?= _('Persönlicher Dateibereich') ?>"
                      :show_bulk_actions="false"
                      :tfoot_link="<?= htmlReady(json_encode($tfoot_link)) ?>"
         ></files-table>
