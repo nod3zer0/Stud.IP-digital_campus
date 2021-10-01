fcd541ef56c39bc01d4143907d4b6b77136849b6
Marcus Eibrink-Lunzenauer <lunzenauer@elan-ev.de>
2021-10-01 10:07:14 +0200

    increase pagination limit when loading course-memberships
    Fixes #245 and #246.

--- resources/vue/store/courseware/courseware.module.js
+++ resources/vue/store/courseware/courseware.module.js
@@ -759,6 +759,7 @@ export const actions = {
 
         const options = {
             include: 'course',
+            'page[limit]': 10000,
         };
 
         return dispatch('course-memberships/loadRelated', { parent, relationship, options }, { root: true }).then(
@@ -813,6 +814,7 @@ export const actions = {
 
         const options = {
             include: 'course',
+            'page[limit]': 10000,
         };
 
         return dispatch('course-memberships/loadRelated', { parent, relationship, options }, { root: true }).then(
