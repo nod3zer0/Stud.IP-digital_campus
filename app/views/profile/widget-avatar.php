d593a94d6a1fbcdcc963c1394e2e5bdbda15b2c9
Moritz Strohm <strohm@data-quest.de>
2021-10-04 11:22:07 +0000

    BIESt #100

--- app/views/profile/widget-avatar.php
+++ app/views/profile/widget-avatar.php
@@ -1,19 +1,23 @@
-<div class="text-center avatar-widget">
-    <?= $avatar->getImageTag(Avatar::NORMAL, [
-        'class' => 'profile-avatar',
-        'style' => ''
-    ]) ?>
-    <?php if ($GLOBALS['perm']->have_profile_perm('user', $current_user)) : ?>
-        <div class="avatar-overlay" id="avatar-overlay"
-                accept="image/gif,image/png,image/jpeg" capture="camera"
-                data-max-size="<?= Avatar::MAX_FILE_SIZE ?>"
-                data-message-too-large="<?= _('Die hochgeladene Datei ist zu groß. Bitte wählen Sie ein anderes Bild.') ?>"
-                data-message-unaccepted="<?= _('Die hochgeladene Datei hat falsche Typ. Bitte wählen Sie ein anderes Bild.') ?>">
-            <a href="<?= URLHelper::getURL('dispatch.php/avatar/update/user/' . $current_user) ?>" data-dialog>
-                <span><?= _('Bild hochladen oder löschen.') ?> <br> <?= _('Drag & Drop oder Klicken') ?></span>
-            </a>
-        </div>
-    <?php endif ?>
+<div class="avatar-widget">
+    <? if ($GLOBALS['perm']->have_profile_perm('user', $current_user)) : ?>
+        <a class="profile-avatar"
+           accept="image/gif,image/png,image/jpeg" capture="camera"
+           data-max-size="<?= Avatar::MAX_FILE_SIZE ?>"
+           data-message-too-large="<?= _('Die hochgeladene Datei ist zu groß. Bitte wählen Sie ein anderes Bild.') ?>"
+           data-message-unaccepted="<?= _('Die hochgeladene Datei hat falsche Typ. Bitte wählen Sie ein anderes Bild.') ?>"
+           href="<?= URLHelper::getURL('dispatch.php/avatar/update/user/' . $current_user) ?>" data-dialog>
+            <?= $avatar->getImageTag(Avatar::NORMAL) ?>
+            <div id="avatar-overlay" class="avatar-overlay">
+                <div class="text">
+                    <?= _('Bild hochladen oder löschen.') ?>
+                    <br>
+                    <?= _('Drag & Drop oder Klicken') ?>
+                </div>
+            </div>
+        </a>
+    <? else : ?>
+        <?= $avatar->getImageTag(Avatar::NORMAL) ?>
+    <? endif ?>
 </div>
 <div class="profile-sidebar-details">
     <? if ($kings): ?>
