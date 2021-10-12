76fb4bede1688b1618016467f24fbac3205043b4
Moritz Strohm <strohm@data-quest.de>
2021-10-12 13:54:52 +0200

    course/members/import_autorlist: fixed typos

--- app/views/course/members/import_autorlist.php
+++ app/views/course/members/import_autorlist.php
@@ -3,7 +3,7 @@
 <form action="<?= $controller->url_for('course/members/set_autor_csv')?>" method="post" name="user" data-dialog class="default">
 <?= CSRFProtection::tokenTag() ?>
     <h1>
-        <?=sprintf(_('%s hinzufügen'), htmlReady(get_title_for_status('autor', 1)))?>
+        <?=sprintf(_('%s hinzufügen'), htmlReady(get_title_for_status('autor', 2)))?>
     </h1>
     <fieldset>
         <legend>
@@ -13,12 +13,12 @@
         <label>
             <?=_('Eingabeformat')?>:
 
-            <?= tooltipHtmlIcon(sprintf(_('In das Textfeld <strong>Teilnehmendenliste übernehmen</strong> können Sie eine Liste mit Namen von %s eingeben,
+            <?= tooltipHtmlIcon(_('In das Textfeld <strong>Teilnehmendenliste übernehmen</strong> können Sie eine Liste mit Namen von Studierenden eingeben,
                 die in die Veranstaltung aufgenommen werden sollen. Wählen Sie in der Auswahlbox das gewünschte Format, in dem Sie die Namen eingeben möchten.<br />
                 <strong>Eingabeformat</strong><br/>
                 <strong>Nachname, Vorname &crarr;</strong><br />Geben Sie dazu in jede Zeile den Nachnamen und (optional) den Vornamen getrennt durch ein Komma oder ein Tabulatorzeichen ein.<br />
                 <strong>Nutzername &crarr;</strong><br />Geben Sie dazu in jede Zeile den Stud.IP Nutzernamen ein.<br />
-                <strong>Email &crarr;</strong><br />Geben Sie dazu in jede Zeile die Email Adresse ein.'), htmlReady($status_groups['autor'])));?>
+                <strong>Email &crarr;</strong><br />Geben Sie dazu in jede Zeile die Email Adresse ein.'));?>
 
             <select name="csv_import_format">
                 <option value="realname"><?=_("Nachname, Vorname")?> &crarr;</option>
@@ -33,7 +33,7 @@
         </label>
 
         <label>
-            <?= sprintf(_('<strong>%s</strong> in die Veranstaltung eintragen'), htmlReady(get_title_for_status('autor', 1)))?></td>
+            <?= sprintf(_('<strong>%s</strong> in die Veranstaltung eintragen'), htmlReady(get_title_for_status('autor', 2)))?></td>
             <textarea name="csv_import" rows="6" cols="50"></textarea>
         </label>
     </fieldset>
