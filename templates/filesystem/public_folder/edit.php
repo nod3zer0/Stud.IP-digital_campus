4047f83cd1656966100e1584baff3ad085c7b9b7
Michaela Brückner <brueckner@data-quest.de>
2021-10-20 06:38:04 +0000

    Biest#103

--- templates/filesystem/public_folder/edit.php
+++ templates/filesystem/public_folder/edit.php
@@ -1,4 +1,4 @@
-<label>
-    <input type="checkbox" name="public_folder_viewable" value="1" <? if ($public_folder_viewable) echo 'checked'; ?>>
+<label for="public_folder_viewable">
+    <input id="public_folder_viewable" type="checkbox" name="public_folder_viewable" value="1" <? if ($public_folder_viewable) echo 'checked'; ?>>
     <?= _('Sichtbar auf der Profilseite') ?>
 </label>
