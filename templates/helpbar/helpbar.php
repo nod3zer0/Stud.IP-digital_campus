21be2069a6865a78fd739bc8524306f494da5e7e
Moritz Strohm <strohm@data-quest.de>
2022-01-07 12:18:58 +0000

    fix for BIESt #522

--- templates/helpbar/helpbar.php
+++ templates/helpbar/helpbar.php
@@ -1,35 +1,35 @@
 <div class="helpbar-container">
     <?= SkipLinks::addIndex(_('Tipps & Hilfe'), 'helpbar_icon', 920) ?>
     <a id="helpbar_icon" href="#" class="helpbar-toggler" data-toggles=".helpbar" title="<?= _('Hilfelasche anzeigen/verstecken') ?>">
-        <?= Icon::create('question-circle')->asImg(24) ?>
+        <?= Icon::create('question-circle')->asImg(24, ['alt' => '']) ?>
     </a>
     <div class="helpbar" <? if (!$open) echo 'style="display: none"'; ?>>
 
         <h2 class="helpbar-title">
             <?= _('Tipps & Hilfe') ?>
             <a href="#" class="helpbar-toggler" data-toggles=".helpbar" aria-hidden="true" title="<?= _('Hilfelasche verstecken') ?>">
-                <?= Icon::create('decline-circle', Icon::ROLE_INFO_ALT)->asImg(24) ?>
+                <?= Icon::create('decline-circle', Icon::ROLE_INFO_ALT)->asImg(24, ['alt' => '']) ?>
             </a>
         </h2>
         <ul class="helpbar-widgets">
         <? foreach ($widgets as $index => $widget): ?>
             <li>
             <? if ($widget->icon): ?>
-                <?= is_string($widget->icon) ? Assets::img($widget->icon, ['class' => 'helpbar-widget-icon']) : $widget->icon->asImg(['class' => 'helpbar-widget-icon']) ?>
+                <?= is_string($widget->icon) ? Assets::img($widget->icon, ['class' => 'helpbar-widget-icon']) : $widget->icon->asImg(['class' => 'helpbar-widget-icon', 'alt' => '']) ?>
             <? endif; ?>
                 <?= $widget->render(['base_class' => 'helpbar'])?>
                 <div class="helpbar-widget-admin-icons">
                 <? if ($widget->edit_link): ?>
                     <a href="<?=$widget->edit_link?>" data-dialog="size=auto;reload-on-close">
-                    <?= Icon::create('edit', 'info_alt')->asImg() ?></a>
+                    <?= Icon::create('edit', 'info_alt')->asImg(['title' => _('Hilfe-Text bearbeiten'), 'alt' => '']) ?></a>
                 <? endif; ?>
                 <? if ($widget->delete_link): ?>
                     <a href="<?=$widget->delete_link?>" data-dialog="size=auto;reload-on-close">
-                    <?= Icon::create('trash', 'info_alt')->asImg() ?></a>
+                    <?= Icon::create('trash', 'info_alt')->asImg(['title' => _('Hilfe-Text löschen'), 'alt' => '']) ?></a>
                 <? endif; ?>
                 <? if ($widget->add_link): ?>
                     <a href="<?=$widget->add_link?>" data-dialog="size=auto;reload-on-close">
-                    <?= Icon::create('add', 'info_alt')->asImg() ?></a>
+                    <?= Icon::create('add', 'info_alt')->asImg(['title' => _('Neuer Hilfe-Text'), 'alt' => '']) ?></a>
                 <? endif; ?>
                 </div>
             </li>
