1fdf82198c3a861e79b4627bfbb121a246a54a87
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2022-02-11 08:33:52 +0000

    fixes #470

---
+++ db/migrations/5.1.17_missing_indices_v50.php
@@ -0,0 +1,39 @@
+<?php
+final class MissingIndicesV50 extends Migration
+{
+    public function up()
+    {
+        $query = "CREATE INDEX `range_id` ON `mvv_files_ranges` (`range_id`)";
+        DBManager::get()->exec($query);
+
+        $query = "CREATE INDEX `context_query` ON `activities` (`context`, `context_id`, `mkdate`)";
+        DBManager::get()->exec($query);
+
+        $query = "CREATE INDEX `user_id` ON `comments` (`user_id`)";
+        DBManager::get()->exec($query);
+
+        $query = "CREATE INDEX `user_id` ON `file_refs` (`user_id`)";
+        DBManager::get()->exec($query);
+
+        $query = "CREATE INDEX `user_id` ON `news` (`user_id`)";
+        DBManager::get()->exec($query);
+    }
+
+    public function down()
+    {
+        $query = "ALTER TABLE `news` DROP INDEX `user_id`";
+        DBManager::get()->exec($query);
+
+        $query = "ALTER TABLE `file_refs` DROP INDEX `user_id`";
+        DBManager::get()->exec($query);
+
+        $query = "ALTER TABLE `comments` DROP INDEX `user_id`";
+        DBManager::get()->exec($query);
+
+        $query = "ALTER TABLE `activities` DROP INDEX `context_query`";
+        DBManager::get()->exec($query);
+
+        $query = "ALTER TABLE `mvv_files_ranges` DROP INDEX `range_id`";
+        DBManager::get()->exec($query);
+    }
+}
