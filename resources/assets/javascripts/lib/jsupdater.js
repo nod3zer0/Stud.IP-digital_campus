7a0b08e6934522cb80505470fa9c4da3eeef109d
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2022-03-11 16:33:55 +0100

    Revert "Biest 748"
    This reverts commit 5386216296ab62d0d8dc447bac9240caf7943e2f.

--- resources/assets/javascripts/lib/jsupdater.js
+++ resources/assets/javascripts/lib/jsupdater.js
@@ -174,11 +174,6 @@ function poll(forced) {
                     $(this).dialog('close');
                 };
 
-                var dialogwidth = '50%';
-                if ($('html').is('.responsive-display')) {
-                    dialogwidth = '85%';
-                }
-
                 $('<div>')
                     .html(message)
                     .css({
@@ -186,13 +181,11 @@ function poll(forced) {
                         padding: '2em 0'
                     })
                     .dialog({
-                        width: dialogwidth,
+                        width: '50%',
                         modal: true,
                         buttons: buttons,
                         title: $gettext('Sie sind nicht mehr im System angemeldet.')
                     });
-
-                $('.ui-dialog-title').css('white-space', 'unset');
             } else {
                 registerNextPoll();
             }
