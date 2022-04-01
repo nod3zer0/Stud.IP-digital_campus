f276e32ba949bf300c6052b358490faa41a5c852
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2022-04-01 15:17:16 +0200

    also allow tutors to copy courseware contents, fixes #856

--- resources/vue/store/courseware/courseware.module.js
+++ resources/vue/store/courseware/courseware.module.js
@@ -961,7 +961,7 @@ export const actions = {
         let courses = [];
         for (let membership of memberships) {
             if (
-                membership.attributes.permission === 'dozent' &&
+                membership.attributes.permission === 'tutor' &&
                 state.context.id !== membership.relationships.course.data.id
             ) {
                 const course = rootGetters['courses/related']({ parent: membership, relationship: 'course' });
@@ -974,7 +974,10 @@ export const actions = {
                     rangeType: course.type
                 });
                 if (coursewareInstance?.relationships?.root) {
-                    courses.push(course);
+                    if (membership.attributes.permission === 'dozent' ||
+                        coursewareInstance.attributes['editing-permission-level'] === 'tutor') {
+                        courses.push(course);
+                    }
                 }
             }
         }
