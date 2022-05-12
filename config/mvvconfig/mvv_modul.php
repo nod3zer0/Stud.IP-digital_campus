cd8d499047babe7af85777ac9dbc2954363a760a
Peter Thienel <thienel@data-quest.de>
2022-05-12 10:06:03 +0000

    Resolve "In den Rechtekonfigurationen für MVV fehlt an Modulen und Studiengangteilen der Eintrag für zugeordnete Personen"
    Closes #271
    
    Merge request studip/studip!91

--- config/mvvconfig/mvv_modul.php
+++ config/mvvconfig/mvv_modul.php
@@ -8,7 +8,7 @@
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
+ *
  * @author      Peter Thienel <thienel@data-quest.de>
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
  * @category    Stud.IP
@@ -19,33 +19,33 @@
 /**
  * Permissions
  * ============
- * 
+ *
  * read: MVVPlugin::PERM_READ | 1
  * read && write: MVVPlugin::PERM_WRITE | 3
  * read && write && create && delete: MVVPlugin::PERM_CREATE | 7
- * 
+ *
  * Structure
  * ==========
- * 
+ *
  * ['default_table' => [name_of_role => permission]]
  * Permissions for the object itself regardless of its status.
  * Every tuple defines the permission for a different role (the role of a user
  * who wants to handle this object).
- * 
+ *
  * ['default_fields' => [name_of_role => permission]]
  * Default permissions for all fields of this object regardless of its status.
  * Maybe overwritten by an entry for a single field.
  * Every tuple defines the permission for a different role (the role of a user
  * who wants to handle this object).
- * 
+ *
  * ['fields' => ... ]
  * Permissions for a single field of this object (db_fields and relations of
  * the SORM-object). Overwites above declaration for this field.
- * 
+ *
  * ['fields' => name_of_field ['default' => [name_of_role => permission]]]
  * Default permission for one field for every given role regardless of
  * object's status.
- * 
+ *
  * ['fields' => name_of_field [name_of_status => [name_of_role => permission]]]
  * Permission for one field of the object with indicated status for every
  * given role. Overwrites above declaration.
@@ -419,16 +419,16 @@ $privileges = [
                 'MVVFreigabe'   => 7
             ]
         ],
-        'assigned_users' => [
+        'contact_assignments' => [
             'planung' => [
                 'MVVEntwickler' => 7,
-                'MVVRedakteur'  => 3,
+                'MVVRedakteur'  => 7,
                 'MVVTranslator' => 1,
                 'MVVFreigabe'   => 7
             ],
             'genehmigt' => [
                 'MVVEntwickler' => 7,
-                'MVVRedakteur'  => 3,
+                'MVVRedakteur'  => 7,
                 'MVVTranslator' => 1,
                 'MVVFreigabe'   => 7
             ]
@@ -513,7 +513,7 @@ $privileges = [
         ],
         // folgende Konfigurationen gelten für bestimmte Funktionen,
         // die auf das Objekt angewendet werden dürfen (oder eben nicht).
-        
+
         // Kopieren von Modulen
         'copy_module' => [
             'planung' => [
