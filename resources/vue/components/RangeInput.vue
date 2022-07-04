affad0b8a04a3bf170ad26d079bc7215dbd8c1ef
Jan-Hendrik Willms <tleilax+github@gmail.com>
2022-07-04 15:16:13 +0200

    fixes #1156

--- resources/vue/components/RangeInput.vue
+++ resources/vue/components/RangeInput.vue
@@ -11,7 +11,7 @@
                v-bind="$attrs"
                v-on="$listeners"
                v-model="myValue">
-        <output for="fader"><translate :translate-params="{myValue: myValue ?? '1', max: max}">%{myValue} von %{max}</translate></output>
+        <output for="fader"><translate :translate-params="{myValue: myValue || '1', max: max}">%{myValue} von %{max}</translate></output>
     </div>
 </template>
 
