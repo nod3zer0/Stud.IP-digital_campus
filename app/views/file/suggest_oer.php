8457640a75f54b04da8110a7a097d95f035f6c7c
Michaela Brückner <brueckner@data-quest.de>
2022-07-05 09:06:17 +0200

    re #852 re #1204

--- app/views/file/suggest_oer.php
+++ app/views/file/suggest_oer.php
@@ -1,21 +1,22 @@
-<form action='<?= $controller->url_for('file/suggest_oer/' . $file_ref_id)?>'
-      class='default' method='POST' data-dialog="reload-on-close">
-    <?= CSRFProtection::tokenTag() ?>
+<div id="file_suggest_oer">
+    <form action='<?= $controller->url_for('file/suggest_oer/' . $file_ref_id)?>'
+          class='default' method='POST' data-dialog="reload-on-close">
+        <?= CSRFProtection::tokenTag() ?>
+        <p><?= sprintf(_('Das Material gefällt Ihnen? – Schlagen Sie es zur Veröffentlichung im OER-Campus vor.
+        Schreiben Sie dem/der Autor*in %s, warum er/sie das Material veröffentlichen sollte.'), $author_fullname)?></p>
+        <label for="additional_text">
+            <span class="">
+                <?= _("Ihr Vorschlag wird anonym versendet.") ?>
+            </span>
+            <textarea   name  = "additional_text"
+                        id    = "additional_text"
+                        rows  = "3"
+            ></textarea>
+        </label>
 
-    <p><?= sprintf(_('Das folgende Material wird %s zur Veröffentlichung im OER Campus vorgeschlagen:'), $author_fullname)?></p>
-    <p><?= htmlReady($file->getFilename())?></p>
-    <label for="additional_text">
-        <span class="">
-            <?= _("Ihr Vorschlag wird anonym versendet.") ?>
-            <?= _("Falls gewünscht, können Sie zusätzlich eine Nachricht verfassen:") ?>
-        </span>
-        <textarea   name  = "additional_text"
-                    id    = "additional_text"
-                    rows  = "3"
-        ></textarea>
-    </label>
-
-    <footer data-dialog-button>
-        <?= Studip\Button::create(_("Material vorschlagen"))?>
-    </footer>
-</form>
+        <footer data-dialog-button>
+            <?= Studip\Button::create(_("Material vorschlagen"))?>
+        </footer>
+    </form>
+</div>
+<?= $this->render_partial('file/file_details.php') ?>
