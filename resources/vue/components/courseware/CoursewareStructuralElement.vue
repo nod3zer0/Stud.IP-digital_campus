96729334f72a167dfb64eaf8d18319ff10c7fd2f
Michaela Brückner <brueckner@data-quest.de>
2022-08-09 08:46:57 +0200

    fix !628 #1061

--- resources/vue/components/courseware/CoursewareStructuralElement.vue
+++ resources/vue/components/courseware/CoursewareStructuralElement.vue
@@ -528,7 +528,7 @@
                 >
                     <template v-slot:dialogContent>
                         <p><translate>Das folgende Courseware-Material wird {{ ownerName }}
-                            zur Veröffentlichung im OER Campus vorgeschlagen:</translate></p>
+                            zur Veröffentlichung im {{ oerTitle }} vorgeschlagen:</translate></p>
                         <table class="cw-structural-element-info">
                             <tr>
                                 <td><translate>Titel</translate>:</td>
@@ -749,7 +749,7 @@ export default {
 
         textSuggestOer() {
             return {
-                title: this.$gettext('Material für OER Campus vorschlagen'),
+                title: this.$gettextInterpolate('Material für %{ oerTitle } vorschlagen', {oerTitle: this.oerTitle}),
                 confirm: this.$gettext('Material vorschlagen'),
                 close: this.$gettext('Schließen'),
             };
@@ -1539,7 +1539,7 @@ export default {
             this.closeLinkDialog();
         },
         closeLinkDialog() {
-            this.publicLink = { 
+            this.publicLink = {
                 passsword: '',
                 'expire-date': ''
             };
