12f24e4f7345f93a114120c508ae020759d4c903
Marcus Eibrink-Lunzenauer <lunzenauer@elan-ev.de>
2022-10-12 11:55:45 +0000

    Benachrichtigungen, wenn `webpack-watch` Fehler zeigt
    Merge request studip/studip!1070

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,26 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const path = require('path');
+
+const statusesPaths = {
+    success: path.join(__dirname, 'public/assets/images/favicon-64x64.png'),
+    error: path.join(__dirname, 'public/assets/images/virtual.png'),
+};
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "eval",
+    mode: 'development',
+    devtool: 'eval',
+    plugins: [
+        new WebpackNotifierPlugin({
+            appID: 'Stud.IP Webpack',
+            title: function (params) {
+                return `Build status is ${params.status}`;
+            },
+            timeout: false,
+            hint: process.platform === 'linux' ? 'int:transient:1' : undefined,
+            excludeWarnings: true,
+            contentImage: statusesPaths,
+        }),
+    ],
 });
