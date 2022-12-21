eb834236e0b596ec8feef170a339d6c7d42a7cd6
Jan-Hendrik Willms <tleilax+github@gmail.com>
2022-12-21 14:51:46 +0100

    Revert "add tooltip content as aria label, fixes #1916"
    This reverts commit 2c31279a42b81ac49cb089e3ce6df72593eede6b.

--- resources/assets/javascripts/bootstrap/tooltip.js
+++ resources/assets/javascripts/bootstrap/tooltip.js
@@ -37,7 +37,6 @@ $(document).on('mouseenter mouseleave focusin focusout', '[data-tooltip],.toolti
         }
         $(this).attr('title', '');
         $(this).attr('data-tooltip', content);
-        $(this).attr('aria-label', content);
 
         tooltip = new STUDIP.Tooltip(x, y, content);
 
--- templates/shared/tooltip.php
+++ templates/shared/tooltip.php
@@ -1,10 +1,3 @@
-<?php
-/**
- * @var bool $html
- * @var bool $important
- * @var string $text
- */
-?>
 <span class="tooltip tooltip-icon <? if ($important) echo 'tooltip-important'; ?>" data-tooltip <? if (!$html) printf('title="%s"', htmlReady($text)) ?> tabindex="0">
 <? if ($html): ?>
     <span class="tooltip-content"><?= $text ?></span>
