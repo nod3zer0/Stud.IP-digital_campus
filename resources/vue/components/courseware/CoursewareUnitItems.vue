c1f749dd68102333153d2c5fab987d79a03a4327
Ron Lucke <lucke@elan-ev.de>
2023-01-10 08:18:02 +0000

    fix #1992
    Closes #1992
    
    Merge request studip/studip!1300

--- resources/vue/components/courseware/CoursewareUnitItems.vue
+++ resources/vue/components/courseware/CoursewareUnitItems.vue
@@ -3,29 +3,31 @@
         <ul v-if="hasUnits" class="cw-tiles">
             <courseware-unit-item v-for="unit in units" :key="unit.id" :unit="unit"/>
         </ul>
-        <div v-if="!hasUnits && userIsTeacher && inCourseContext" class="cw-contents-overview-teaser">
-            <div class="cw-contents-overview-teaser-content">
-                <header>{{ $gettext('Lernmaterialien') }}</header>
-                <p>
-                    {{ $gettext('Mit Courseware können Sie interaktive, multimediale Lerninhalte erstellen und nutzen. ' +
-                                'Die Lerninhalte lassen sich hierarchisch unterteilen und können aus Texten, Videosequenzen, ' +
-                                'Aufgaben, Kommunikationselementen und einer Vielzahl weiterer Elemente bestehen. ' +
-                                'Fertige Lerninhalte können exportiert und in andere Kurse oder andere Installationen importiert werden. ' +
-                                'Courseware ist nicht nur für digitale Formate geeignet, sondern kann auch genutzt werden, ' +
-                                'um klassische Präsenzveranstaltungen mit Online-Anteilen zu ergänzen. Formate wie integriertes Lernen ' +
-                                '(Blended Learning) lassen sich mit Courseware ideal umsetzen. Kollaboratives Lernen kann dank Schreibrechtevergabe ' +
-                                'und dem Einsatz von Courseware in Studiengruppen realisiert werden.') }}
-                </p>
-                <button class="button" @click="setShowUnitAddDialog(true)">
-                    {{ $gettext('Neues Lernmaterial anlegen') }}
-                </button>
+        <template v-if="!hasUnits && inCourseContext">
+            <div v-if="userIsTeacher" class="cw-contents-overview-teaser">
+                <div class="cw-contents-overview-teaser-content">
+                    <header>{{ $gettext('Lernmaterialien') }}</header>
+                    <p>
+                        {{ $gettext('Mit Courseware können Sie interaktive, multimediale Lerninhalte erstellen und nutzen. ' +
+                                    'Die Lerninhalte lassen sich hierarchisch unterteilen und können aus Texten, Videosequenzen, ' +
+                                    'Aufgaben, Kommunikationselementen und einer Vielzahl weiterer Elemente bestehen. ' +
+                                    'Fertige Lerninhalte können exportiert und in andere Kurse oder andere Installationen importiert werden. ' +
+                                    'Courseware ist nicht nur für digitale Formate geeignet, sondern kann auch genutzt werden, ' +
+                                    'um klassische Präsenzveranstaltungen mit Online-Anteilen zu ergänzen. Formate wie integriertes Lernen ' +
+                                    '(Blended Learning) lassen sich mit Courseware ideal umsetzen. Kollaboratives Lernen kann dank Schreibrechtevergabe ' +
+                                    'und dem Einsatz von Courseware in Studiengruppen realisiert werden.') }}
+                    </p>
+                    <button class="button" @click="setShowUnitAddDialog(true)">
+                        {{ $gettext('Neues Lernmaterial anlegen') }}
+                    </button>
+                </div>
             </div>
-        </div>
-        <courseware-companion-box
-            v-if="!userIsTeacher && inCourseContext"
-            :msgCompanion="$gettext('Es wurden leider noch keine Lernmaterialien angelegt.')"
-            mood="sad"
-        />
+            <courseware-companion-box
+                v-else
+                :msgCompanion="$gettext('Es wurden leider noch keine Lernmaterialien angelegt.')"
+                mood="sad"
+            />
+        </template>
         <div v-if="!hasUnits && !inCourseContext" class="cw-contents-overview-teaser">
             <div class="cw-contents-overview-teaser-content">
                 <header>{{ $gettext('Ihre persönlichen Lernmaterialien') }}</header>
