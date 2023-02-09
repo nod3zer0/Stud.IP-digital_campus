ffabf00faba64ff84a17592250bf695ba79eeff5
Thomas Hackl <hackl@data-quest.de>
2023-02-09 10:12:12 +0000

    Resolve "Wiki/Courseware: Inhaltsverzeichnis hängt falsch"
    Closes #2138, #2139, and #2130
    
    Merge request studip/studip!1383

--- resources/assets/javascripts/bootstrap/fullscreen.js
+++ resources/assets/javascripts/bootstrap/fullscreen.js
@@ -1,22 +1,28 @@
 STUDIP.ready(() => {
-    $('#fullscreen-on').on('click', event => {
+    document.getElementById('fullscreen-on').addEventListener('click', event => {
         event.preventDefault();
         STUDIP.Vue.emit('toggle-compact-navigation', true);
     });
-    $('#fullscreen-off').on('click', event => {
+
+    document.getElementById('fullscreen-off').addEventListener('click', event => {
         event.preventDefault();
         STUDIP.Vue.emit('toggle-compact-navigation', false);
-    });
-    $('#focusmode-on, .fullscreen-trigger').on('click', event => {
-        event.preventDefault();
-        STUDIP.Fullscreen.activate();
-    });
+    })
+
+    for (const elem of document.querySelectorAll('#focusmode-on, .fullscreen-trigger')) {
+        elem.addEventListener('click', event => {
+            event.preventDefault();
+            STUDIP.Fullscreen.activate();
+        });
+    }
+
     // Listen for fullscreen exit, ending focus mode with it.
     document.addEventListener('fullscreenchange', event => {
         if (!document.fullscreenElement) {
             STUDIP.Fullscreen.deactivate();
         }
     });
+
     // Fullscreen exit on Safari
     document.addEventListener('webkitfullscreenchange', event => {
         if (!document.webkitFullscreenElement) {
