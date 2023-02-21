6a20f2aebb96ed0d9816c5da9903946abe11284f
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2023-02-21 17:19:46 +0000

    adjust order of element, fixes #1724
    Closes #1724
    
    Merge request studip/studip!1403

--- templates/sidebar/widget-layout.php
+++ templates/sidebar/widget-layout.php
@@ -1,14 +1,31 @@
-<div class="<?= $base_class ?>-widget <?= is_array($layout_css_classes) ? htmlReady(implode(' ', $layout_css_classes)) : '' ?>"
-    <?= arrayToHtmlAttributes($additional_attributes) ?>>
+<?php
+/**
+ * @var string $title
+ * @var string $base_class
+ * @var array $additional_attributes
+ * @var string|null $extra
+ * @var string $content
+ */
+
+$css_classes = $layout_css_classes ?? [];
+$css_classes[] = "{$base_class}-widget";
+
+if ($title && isset($extra)) {
+    $css_classes[] = 'sidebar-widget-has-extra';
+}
+
+$additional_attributes['class'] = implode(' ', $css_classes);
+?>
+<div <?= arrayToHtmlAttributes($additional_attributes) ?>>
 <? if ($title): ?>
     <div class="<?= $base_class ?>-widget-header">
-    <? if (isset($extra)): ?>
-        <div class="<?= $base_class ?>-widget-extra"><?= $extra ?></div>
-    <? endif; ?>
         <?= htmlReady($title) ?>
     </div>
 <? endif; ?>
     <div class="<?= $base_class ?>-widget-content">
         <?= $content_for_layout ?>
     </div>
+<? if ($title && isset($extra)): ?>
+    <div class="<?= $base_class ?>-widget-extra"><?= $extra ?></div>
+<? endif; ?>
 </div>
