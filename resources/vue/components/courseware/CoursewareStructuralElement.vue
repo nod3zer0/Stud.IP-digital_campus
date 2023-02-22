69276f81ebf2ceb57ba8853c93d75ab50401c527
Ron Lucke <lucke@elan-ev.de>
2023-02-22 09:11:26 +0000

    fix #2178
    Closes #2178
    
    Merge request studip/studip!1439

--- resources/vue/components/courseware/CoursewareStructuralElement.vue
+++ resources/vue/components/courseware/CoursewareStructuralElement.vue
@@ -119,7 +119,7 @@
                     </div>
                     <div
                         v-if="isLink"
-                        class="container-wrapper"
+                        class="cw-container-wrapper"
                         :class="{
                             'cw-container-wrapper-consume': consumeMode,
                             'cw-container-wrapper-discuss': discussView,
