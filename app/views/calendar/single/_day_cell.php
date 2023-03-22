d99ab61e19afda87bd0f13e547547404a9afe97d
Peter Thienel <thienel@data-quest.de>
2023-03-22 10:14:45 +0000

    Resolve "Endzeit von Terminen in der Tages- und Wochenansicht falsch"
    Closes #2404
    
    Merge request studip/studip!1600

--- app/views/calendar/single/_day_cell.php
+++ app/views/calendar/single/_day_cell.php
@@ -13,16 +13,17 @@
 <? else : ?>
     <? for ($j = 0; $j < $em['colsp'][$row]; $j++) : ?>
         <? $event = $em['term'][$row][$j]; ?>
+        <? $mapped_event = $calendar->events[$em['mapping'][$row][$j]]; ?>
         <? if (is_object($event)) : ?>
-    <td data-tooltip<?= ($em['cspan'][$row][$j] > 1 ? ' colspan="' . $em['cspan'][$row][$j] . '"' : '') ?><?= ($em['rows'][$row][$j] > 1 ? ' rowspan="' . $em['rows'][$row][$j] . '"' : '') ?> class="<?= $event instanceof CourseEvent ? 'calendar-course-category' : 'calendar-category' ?><?= $event->getCategory() ?> calendar-day-event">
+            <td data-tooltip<?= ($em['cspan'][$row][$j] > 1 ? ' colspan="' . $em['cspan'][$row][$j] . '"' : '') ?><?= ($em['rows'][$row][$j] > 1 ? ' rowspan="' . $em['rows'][$row][$j] . '"' : '') ?> class="<?= $event instanceof CourseEvent ? 'calendar-course-category' : 'calendar-category' ?><?= $event->getCategory() ?> calendar-day-event">
                 <? if ($em['rows'][$row][$j] > 1) : ?>
-                <div>
-                    <?= date('H.i-', $event->getStart()) . date('H.i', $event->getEnd()) ?>
-                </div>
+                    <div>
+                        <?= date('H.i-', $mapped_event->getStart()) . date('H.i', $mapped_event->getEnd()) ?>
+                    </div>
                 <? endif ?>
                 <div class="calendar-day-event-title">
                     <a title="<?= _('Termin bearbeiten') ?>" href="<?= $controller->url_for('calendar/single/edit/' . $calendar->getRangeId() . '/' . $event->event_id, ['atime' => $atime_new, 'evtype' => $event->getType()]) ?>"><?= htmlReady($event->getTitle()) ?></a>
-                    <?= $this->render_partial('calendar/single/_tooltip', ['event' => $event]) ?>
+                    <?= $this->render_partial('calendar/single/_tooltip', ['event' => $mapped_event]) ?>
                 </div>
             </td>
         <? elseif ($event == '#') : ?>
