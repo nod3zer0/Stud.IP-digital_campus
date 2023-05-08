fd50b7a4f82b37d75b9bfe2cfa560df0c6710845
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2023-05-08 12:10:53 +0000

    fixes #2613
    Closes #2613
    
    Merge request studip/studip!1763

--- templates/calendar/entries/icons.php
+++ templates/calendar/entries/icons.php
@@ -1,6 +1,12 @@
+<?php
+/**
+ * @var string $element_id
+ * @var array $entry
+ */
+?>
 <div id="schedule_icons_<?= $element_id ?>" class="schedule_icons">
     <? if (!empty($entry['icons'])) foreach ($entry['icons'] as $icon) : ?>
-        <? if($icon['url']) : ?>
+        <? if (!empty($icon['url'])) : ?>
         <a href="<?= $icon['url'] ?>" <?= $icon['onClick'] ? 'onClick="STUDIP.Calendar.clickEngine('. $icon['onClick'].', this, event); return false;"' : '' ?>>
             <?= Assets::img($icon['image'], ['title' => $icon['title'], 'alt' => $icon['title']]) ?>
         </a>
