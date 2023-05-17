29613caf23c53e2a137ace3cf74e0af7836aa4aa
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2023-05-17 13:11:00 +0000

    make ckeditor resizable, re #2525
    Merge request studip/studip!1712

--- resources/assets/javascripts/bootstrap/wysiwyg.js
+++ resources/assets/javascripts/bootstrap/wysiwyg.js
@@ -3,6 +3,15 @@ STUDIP.domReady(() => {
         // replace areas visible on page load
         replaceVisibleTextareas();
 
+        // update the height of the editor view on focus changes in dialogs
+        $(document).on('focus blur', '.studip-dialog .ck-editor__editable_inline', function(event) {
+            let height = this.clientHeight;
+            let editor = this.ckeditorInstance;
+            editor.editing.view.change(writer => {
+                writer.setStyle('height', height + 'px', editor.editing.view.document.getRoot());
+            });
+        });
+
         // replace areas that are created or shown after page load
         // remove editors that become hidden after page load
         // show, hide and create do not raise an event, use interval timer
