fff82e7148bcfa3c3e3eef2ed37a312549a5f48c
Jan-Hendrik Willms <tleilax+studip@gmail.com>
2023-06-05 15:15:23 +0000

    don't lose the toolbar, fixes #2373
    Closes #2373
    
    Merge request studip/studip!1824

--- resources/vue/components/TextareaWithToolbar.vue
+++ resources/vue/components/TextareaWithToolbar.vue
@@ -1,15 +1,28 @@
 <template>
-    <textarea :name="name" ref="textarea"
-              v-bind="$attrs" v-on="$listeners"></textarea>
+    <textarea :name="name"
+              ref="textarea"
+              v-model="content"
+    ></textarea>
 </template>
 
 <script>
 export default {
     name: 'textarea-with-toolbar',
     props: {
+        value: String,
         name: {
             type: String,
             required: false
+        }
+    },
+    computed: {
+        content: {
+            get() {
+                return this.value;
+            },
+            set(newContent) {
+                this.$emit('input', newContent);
+            }
         }
     },
     mounted () {
