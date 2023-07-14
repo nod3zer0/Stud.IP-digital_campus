614c41a1bdf7db81addc99f061c08417de229fff
David Siegfried <david.siegfried@uni-vechta.de>
2023-07-14 20:02:52 +0000

    fix redirect url, closes #2846
    Closes #2846
    
    Merge request studip/studip!1935

--- lib/modules/CoreOverview.class.php
+++ lib/modules/CoreOverview.class.php
@@ -41,8 +41,9 @@ class CoreOverview extends CorePlugin implements StudipModule
         }
 
         $nav = new Navigation(_('Ankündigungen'), '');
+        $url_params = ['redirect_to' => 'dispatch.php/course/overview'];
         if ($result['neue']) {
-            $nav->setURL('?new_news=true');
+            $url_params['new_news'] = true;
             $nav->setImage(Icon::create('news', Icon::ROLE_ATTENTION), [
                 'title' => sprintf(
                     ngettext(
@@ -67,6 +68,7 @@ class CoreOverview extends CorePlugin implements StudipModule
                 )
             ]);
         }
+        $nav->setURL(URLHelper::getURL('', $url_params));
         return $nav;
     }
 
