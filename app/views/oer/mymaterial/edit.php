2bce6f6afe7b037855387dcfc3d2dbd150a535b1
Rasmus Fuhse <fuhse@data-quest.de>
2023-07-19 14:10:21 +0000

    Resolve "OER-Campus: Fehlermeldung im Bearbeiten eigenes Materials"
    Closes #2884
    
    Merge request studip/studip!1975

--- app/views/oer/mymaterial/edit.php
+++ app/views/oer/mymaterial/edit.php
@@ -70,7 +70,7 @@
             <? if (empty($_SESSION['NEW_OER']['tmp_name'])) : ?>
                 <label class="file drag-and-drop"
                        data-filename="<?= htmlReady($material['filename']) ?>"
-                       data-filesize="<?= htmlReady(!$material->isNew() ? filesize($material->getFilePath()) : "") ?>"
+                       data-filesize="<?= htmlReady(!$material->isNew() && file_exists($material->getFilePath()) ? filesize($material->getFilePath()) : "") ?>"
                        @drop.prevent="dropFile">
                     <?= _('Datei (gerne auch eine ZIP-Datei) auswählen') ?>
                     <input type="file" name="file" id="oer_file" @change="editFile">
