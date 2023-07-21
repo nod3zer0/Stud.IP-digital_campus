830ead71e21d660a7b2c93d882fd1adc4b20984d
Thomas Hackl <hackl@data-quest.de>
2023-07-21 15:08:08 +0200

    Revert "Sidebar in Veranstaltungen->Administration scrollt nicht mit, closes #2255"
    This reverts commit a70a5f70aadf1cba165c41b33f3485a22979338b.

--- resources/assets/javascripts/entry-base.js
+++ resources/assets/javascripts/entry-base.js
@@ -86,7 +86,6 @@ import "./bootstrap/contentmodules.js"
 import "./bootstrap/responsive-navigation.js"
 import "./bootstrap/treeview.js"
 import "./bootstrap/stock-images.js"
-import "./bootstrap/sidebar.js"
 
 import "./mvv_course_wizard.js"
 import "./mvv.js"
