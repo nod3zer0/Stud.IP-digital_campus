0f2811fcf6f738f73dcc4485263c43c273bea908
Ron Lucke <lucke@elan-ev.de>
2023-12-06 07:42:13 +0000

    TIC #3512
    Closes #3512
    
    Merge request studip/studip!2420

--- resources/vue/components/blubber/Comment.vue
+++ resources/vue/components/blubber/Comment.vue
@@ -1,44 +1,60 @@
 <template>
-    <li :class="commentClass">
-        <a
-            :href="userProfileURL"
-            class="avatar"
-            :title="comment.author['formatted-name']"
-            :style="{ backgroundImage: 'url(' + commentAvatar + ')' }"
-        ></a>
-        <div class="content" :class="{ editing }">
-            <a :href="userProfileURL" class="name">{{ comment.author['formatted-name'] }}</a>
-            <div ref="html" v-html="comment['content-html']" class="html"></div>
-            <textarea
-                ref="textarea"
-                class="edit"
-                v-model="localText"
-                @keydown.enter.exact.prevent="saveComment"
-                @keyup.escape.exact="doneEditing"
-            ></textarea>
+    <li :class="{ 'talk-bubble-own-post': ownComment }" class="talk-bubble-wrapper">
+        <div v-if="!ownComment" class="talk-bubble-avatar">
+            <a :href="userProfileURL" :title="comment.author['formatted-name']">
+                <img :src="commentAvatar" />
+            </a>
         </div>
-        <div class="time">
-            <studip-date-time :timestamp="commentMkdate" :relative="true"></studip-date-time>
-            <a
-                href=""
-                v-if="comment['is-writable']"
-                @click.prevent.stop="editComment"
-                class="edit_comment"
-                :title="$gettext('Bearbeiten.')"
-            >
-                <studip-icon shape="edit" :size="14" role="inactive"></studip-icon>
-            </a>
-            <a href="" @click.prevent="answerComment" class="answer_comment" :title="$gettext('Hierauf antworten.')">
-                <studip-icon shape="export" :size="14" role="inactive"></studip-icon>
-            </a>
+        <div class="talk-bubble" :class="{ editing }">
+            <div class="talk-bubble-content">
+                <header v-if="!ownComment" class="talk-bubble-header">
+                    <a :href="userProfileURL">{{ comment.author['formatted-name'] }}</a>
+                </header>
+                <div class="talk-bubble-talktext">
+                    <template v-if="!editing">
+                        <div ref="html" v-html="comment['content-html']" class="html"></div>
+                        <div class="talk-bubble-footer">
+                            <span class="talk-bubble-talktext-time"><studip-date-time :timestamp="commentMkdate"
+                                    :relative="true"></studip-date-time></span>
+                            <a href="#" v-if="comment['is-writable']" @click.prevent.stop="editComment" class="edit_comment"
+                                :title="$gettext('Bearbeiten')">
+                                <studip-icon shape="edit" :size="14" />
+                            </a>
+                            <a href="#" @click.prevent="answerComment" class="answer_comment"
+                                :title="$gettext('Hierauf antworten')">
+                                <studip-icon shape="reply" :size="14" />
+                            </a>
+
+                        </div>
+                    </template>
+                    <div v-else class="talk-bubble-edit">
+                    <textarea
+                        v-model="localText"
+                        ref="textarea"
+                        @input="setTextareaSize"
+                        @focus="setTextareaSize"
+                        @keydown.enter.exact.prevent="saveComment"
+                        @keyup.escape.exact="doneEditing"
+                    ></textarea>
+                        <button @click="saveComment" :title="$gettext('Speichern')">
+                            <studip-icon shape="accept" />
+                        </button>
+                        <button @click="doneEditing" :title="$gettext('Abbrechen')">
+                            <studip-icon shape="decline" />
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     </li>
 </template>
+
 <script>
 export default {
     name: 'BlubberComment',
     data: () => ({
         localText: '',
+        commentWidth: 0,
     }),
     props: {
         comment: {
@@ -69,6 +85,9 @@ export default {
                 return window.STUDIP.URLHelper.getURL('dispatch.php/profile/extern/' + user_id);
             }
         },
+        ownComment() {
+            return this.comment.isMine();
+        }
     },
     methods: {
         answerComment() {
@@ -100,11 +119,17 @@ export default {
                 this.$emit('remove-comment', this.comment);
             }
         },
+        setTextareaSize() {
+            const textarea = this.$refs.textarea;
+            textarea.style.width = this.commentWidth + 'px';
+            textarea.style.height = textarea.scrollHeight + 'px';
+        }
     },
     mounted() {
         this.resetContent();
         this.$nextTick(() => {
             window.STUDIP.Markup.element(this.$refs.html);
+            this.commentWidth = this.$refs.html.offsetWidth;
         });
     },
     watch: {
