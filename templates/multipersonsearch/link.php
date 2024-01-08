2693d8eaf3eb749414ddbc2cf44ba0c62d1a801a
Elmar Ludwig <elmar.ludwig@uni-osnabrueck.de>
2024-01-08 11:35:56 +0000

    add CSS class action-menu-item-icon, fixes #3599
    Closes #3599
    
    Merge request studip/studip!2494

--- templates/multipersonsearch/link.php
+++ templates/multipersonsearch/link.php
@@ -1,7 +1,7 @@
 <a href="<?= URLHelper::getLink('dispatch.php/multipersonsearch/no_js_form/?name=' . $name); ?>" class="multi_person_search_link" data-dialog="width=720;height=460;id=mp-search"  data-dialogname="<?= $name; ?>" title="<?= htmlReady($title) ?>" data-js-form="<?= URLHelper::getLink('dispatch.php/multipersonsearch/js_form/' . $name); ?>">
     <?
     if (!empty($linkIconPath)) {
-        print $linkIconPath->asImg();
+        print $linkIconPath->asImg(['class' => 'action-menu-item-icon']);
     }
     if (!empty($linkIconPath) && !empty($linkText)) {
         print " ";
