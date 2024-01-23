fdce226282d2421c9cea33e97b0a9c4682f27495
Ron Lucke <lucke@elan-ev.de>
2024-01-23 16:17:27 +0000

    fix #2674
    Closes #2674
    
    Merge request studip/studip!1878

--- resources/vue/components/courseware/layouts/CoursewareCompanionOverlay.vue
+++ resources/vue/components/courseware/layouts/CoursewareCompanionOverlay.vue
@@ -1,11 +1,20 @@
 <template>
-    <div
-        class="cw-companion-overlay"
-        :class="[showCompanion ? 'cw-companion-overlay-in' : '', showCompanion ? '' : 'cw-companion-overlay-out', styleCompanion]"
-        :aria-hidden="!showCompanion"
-    >
-        <div class="cw-companion-overlay-content" v-html="msgCompanion"></div>
-        <button class="cw-compantion-overlay-close" @click="hideCompanion"></button>
+    <div class="cw-companion-overlay-wrapper">
+        <div
+            class="cw-companion-overlay"
+            :class="[showCompanion ? 'cw-companion-overlay-in' : '', showCompanion ? '' : 'cw-companion-overlay-out', styleCompanion]"
+            aria-hidden="true"
+        >
+            <div class="cw-companion-overlay-content" v-html="msgCompanion"></div>
+            <button class="cw-compantion-overlay-close" @click="hideCompanion"></button>
+        </div>
+        <div
+            class="sr-only"
+            aria-live="polite"
+            role="log"
+        >
+            <p>{{ msgCompanion }}</p>
+        </div>
     </div>
 </template>
 
